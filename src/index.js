581481233648512583b95522ea92f71e2079915b
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-04-12 16:35:30 -0400

    added seconds counter feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const $ = require('jquery');
-
-$('#main').html('Here we go!');
+import $ from 'jquery';
+let num = 0;
+setInterval(
+    () => {
+        $('#main').html(`You've been on this page for ${num} seconds!`);
+        num++;
+    },
+    1000
+);
