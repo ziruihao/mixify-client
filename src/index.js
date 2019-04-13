c045966f1ee75c98eb6aa60e5ad8e6b238b6c9ce
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-04-12 21:42:29 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import './style.scss';
 import $ from 'jquery';
-let num = 0;
+
+let sec = 0;
 setInterval(
-    () => {
-        $('#main').html(`You've been on this page for ${num} seconds!`);
-        num++;
-    },
-    1000
+  () => {
+    if (sec >= 60) {
+      const min = Math.floor(sec / 60);
+      $('#main').html(`You've been on this page for ${min} minutes and ${sec % 60} seconds!`);
+    } else {
+      $('#main').html(`You've been on this page for ${sec} seconds!`);
+    }
+    sec += 1;
+  },
+  1000,
 );
