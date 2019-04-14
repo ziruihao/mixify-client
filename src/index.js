aad9f089bf67730345ea2bcbf4916ea6791dbedd
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-04-14 17:26:55 -0400

    setup react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
-import $ from 'jquery';
 
-let sec = 0;
-setInterval(
-  () => {
-    if (sec >= 60) {
-      const min = Math.floor(sec / 60);
-      $('#main').html(`You've been on this page for ${min} minutes and ${sec % 60} seconds!`);
-    } else {
-      $('#main').html(`You've been on this page for ${sec} seconds!`);
-    }
-    sec += 1;
-  },
-  1000,
-);
+const App = () => <div className="test">All the REACTS are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
