66da14b508b3eaf784175e7612a5d9d180e456f0
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-04 23:24:50 -0400

    built action creators to retrieve audio features from Spotify API

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,15 +3,38 @@ import axios from 'axios';
 export const ActionTypes = {
   REQUEST_LOGIN: 'REQUEST_LOGIN',
   SAVE_TOKEN: 'SAVE_TOKEN',
+  GET_AUDIO_FEATURES: 'GET_AUDIO_FEATURES',
 };
 
+// custom-built authentication node.js mixify-server
 const ROOT_URL = 'https://mixify-server.herokuapp.com';
-const API_KEY = '?key=zirui_hao';
+// Spotify's API
+const SPOTIFY_URL = 'https://api.spotify.com/v1';
 
+/**
+ * Saves the user authentication token to redux store and configures axios headers.
+ * @param {String} token
+ */
 export function saveToken(token) {
+  axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
   return {
     type: ActionTypes.SAVE_TOKEN,
     payload: token,
+  };
+}
+
+/**
+ * Grabs cool audio insights from Spotify's API for a song.
+ * @param {String} id
+ */
+export function getAudioFeatures(id) {
+  return (dispatch) => {
+    axios.get(`${SPOTIFY_URL}/audio-features/${id}`).then((response) => {
+      dispatch({ type: ActionTypes.GET_AUDIO_FEATURES, payload: response.data });
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 }
 
