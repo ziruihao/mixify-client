73cedfc70cc87beec05bb03741b69d7ee99807bf
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-05 12:42:16 -0400

    refactored reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import AuthReducer from './auth-reducer';
 import MusicReducer from './music-reducer';
+import MixReducer from './mix-reducer';
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
   music: MusicReducer,
+  mixes: MixReducer,
 });
 
 export default rootReducer;
---
+++ src/reducers/mix-reducer.js
@@ -0,0 +1,35 @@
+import { ActionTypes } from '../actions';
+
+const initialState = {
+  all: [],
+  current: null,
+};
+
+const MixReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.CURRENTIZE_MIX:
+      return (Object.assign({}, state, {
+        current: action.payload,
+      }));
+    case ActionTypes.MAKE_MIX:
+      return (Object.assign({}, state, {
+        current: action.payload,
+      }));
+    case ActionTypes.UPDATE_MIX:
+      return (Object.assign({}, state, {
+        current: action.payload,
+      }));
+    case ActionTypes.REMOVE_MIX:
+      return (Object.assign({}, state, {
+        current: null,
+      }));
+    case ActionTypes.FETCH_MIXES:
+      return (Object.assign({}, state, {
+        all: action.payload,
+      }));
+    default:
+      return state;
+  }
+};
+
+export default MixReducer;
